fdaefc0aae0fe8e4442ae66392b2e2c4e3d6d57d
wqt <318275321@qq.com>
2023-02-20 13:26:10 +0800

    模拟底层 cpu

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
-mod animal;
+///! CPU 最基本的基石
+mod chip_cpu;
 
-use  animal::Cat;
-use  animal::Dog;
+use chip_cpu::CPU;
 
 fn main() {
-    Cat::cat_name();
-    Dog::dog_name();
+    let mut cpu =CPU {
+        operation_code: 0,
+        registers: [0; 2],
+    };
+    cpu.operation_code = 0x8014;
+    cpu.registers[0] = 5;
+    cpu.registers[1] = 10;
+    
+    cpu.run();
+    
+    println!("5 + 10 = {}", cpu.registers[0]);
 }
