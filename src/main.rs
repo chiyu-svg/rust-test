83aced7289c98e1de97f52554ede311332e64feb
wqt <318275321@qq.com>
2023-03-06 22:22:59 +0800

    trait 对象练习

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,63 @@
-use std::error::Error;
-use reqwest;
+use rand;
+use rand::seq::SliceRandom;
+use rand::Rng;
 
-fn main() -> Result<(), Box<dyn Error>>{
-    let url = "https://mbd.baidu.com/newspage/data/landingsuper?context=%7B%22nid%22%3A%22news_9077862423058967403%22%7D&n_type=-1&p_from=-1";
-    let mut response = reqwest::get(url)?;
-    let content = response.text()?;
-    print!("{}", content);
-    Ok(())
+#[derive(Debug)]
+struct Dwarf {}
+
+#[derive(Debug)]
+struct Elf {}
+
+#[derive(Debug)]
+struct Human {}
+
+#[derive(Debug)]
+enum Thing {
+    Sword,
+    Trinket
+}
+
+trait Enchanter: std::fmt::Debug {
+   fn competency(&self) -> f64;
+
+   fn enchant(&self, thing: &mut Thing) {
+        let probability_of_success = self.competency();
+        let spell_is_successful = rand::thread_rng()
+         .gen_bool(probability_of_success);
+        print!("{:?} mutters incoherently.", thing);
+        if spell_is_successful {
+            println!("The {:?} glows brightly.", thing);
+        } else {
+            println!("The {:?} fizees, \
+             then turns into a worthless trinket.", thing);
+            *thing = Thing::Trinket {};
+        }
+   } 
+}
+impl Enchanter for Dwarf {
+    fn competency(&self) -> f64 {
+        0.5
+    }
+}
+impl Enchanter for Elf {
+    fn competency(&self) -> f64 {
+        0.95
+    }
+}
+impl Enchanter for Human {
+    fn competency(&self) -> f64 {
+        0.8
+    }
+}
+
+fn main() {
+    let mut it = Thing::Sword;
+    
+    let d = Dwarf {};
+    let e = Elf {};
+    let h = Human {};
+
+    let party: Vec<&dyn Enchanter> = vec![&d, &e, &h];
+    let spellcaster = party.choose(&mut rand::thread_rng()).unwrap();
+    spellcaster.enchant(&mut it);
 }
