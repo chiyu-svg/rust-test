47f9253d9f33abb2072e6b4cabff1e7e49bb3c19
wqt <318275321@qq.com>
2023-03-08 06:46:07 +0800

    解决不同类型错误

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,27 @@
-use std::net::{SocketAddr, UdpSocket};
-use std::time::Duration; // 准确的获取时间
-use clap::{App, Arg};
-use rand;
-use trust_dns::op::{Message, MessageType, OpCode, Query};
-use trust_dns::rr::domain::Name;
-use trust_dns::rr::record_type::RecordType;
-use trust_dns::serialize::binary::*;
+use std::error;
+use std::fs::File;
+use std::io;
+use std::net::{self, Ipv6Addr};
+use std::fmt;
 
-fn main() {
-    // 创建一个 app
-    let app = App::new("resolve")
-        .about("A simple to use DNS resolver")
-        .arg(Arg::with_name("dns-server").short("s").default_value("1.1.1.1"))
-        .arg(Arg::with_name("domain-name").required(true))
-        .get_matches();
 
-    // 获取域名 和 dns服务器
-    let domain_name_raw = app.value_of("domain-name").unwrap();
-    let domain_name = Name::from_ascii(domain_name_raw).unwrap();
-    let dns_server_raw = app.value_of("dns-server").unwrap();
-    let dns_server: SocketAddr = format!("{}:53", dns_server_raw).parse().expect("invalid address");
 
-    // 请求和响应数据缓存
-    let mut request_as_bytes: Vec<u8> = Vec::with_capacity(512);
-    let mut response_as_bytes: Vec<u8> = vec![0;512];
+#[derive(Debug)]
+enum UpsteamError {
+    IO(io::Error),
+    Parsing(net::AddrParseError)
+}
 
-    let mut msg = Message::new();
-    
-    // 封装 DNS 请求报文
-    msg.set_id(rand::random::<u16>())
-    .set_message_type(MessageType::Query)
-    .add_query(Query::query(domain_name, RecordType::A))
-    .set_op_code(OpCode::Query)
-    .set_recursion_desired(true);
-    
-    // 将 Message 转译成原始字节
-    let mut encoder = BinEncoder::new(&mut request_as_bytes);
-    msg.emit(&mut encoder).unwrap();
-
-    // 创造 Socket
-    let localhost = UdpSocket::bind("0.0.0.0:0").expect("cannot bind to local socket");
-    let timeout = Duration::from_secs(3);
-    localhost.set_read_timeout(Some(timeout)).unwrap();     // 不明白为什么要接受一个 Option
-    localhost.set_nonblocking(false).unwrap();
-     
-    // 发送请求
-    let _amt = localhost.send_to(&request_as_bytes, dns_server).expect("socket misconfigured");
-    // 接受数据
-    let (_amt, _remote) = localhost.recv_from(&mut response_as_bytes).expect("timeout reached");
-    
-    let dns_message = Message::from_vec(&response_as_bytes).expect("unable to parse response");
-    
-    for answer in dns_message.answers() {
-        if answer.record_type() == RecordType::A {
-            let resource = answer.rdata();
-            let ip = resource.to_ip_addr().expect("invalid IP address received");
-            println!("{}", ip.to_string());
-        }
+impl fmt::Display for UpsteamError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:?}", self)
     }
-    
+}
+
+impl error::Error for UpsteamError {}
+
+fn main() -> Result<(), UpsteamError> {
+    let _f = File::open("invisible.txt").map_err(UpsteamError::IO)?;
+    let _localhost = "::1".parse::<Ipv6Addr>().map_err(UpsteamError::Parsing)?;
+    Ok(())
 }
